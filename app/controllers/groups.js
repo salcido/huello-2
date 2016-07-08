adfc1ef798023a3351729aee16644bb452f39214
Matthew Salcido <matthew.salcido@healthsparq.com>
2016-07-08 08:46:21 -0700

    Removed unused controllers; reset group controller state upon group change

--- app/controllers/groups.js
+++ app/controllers/groups.js
@@ -8,7 +8,14 @@ export default Ember.Controller.extend({
 
       this.send('updateModel', value.id);
 
+      // Show app overlay
       Ember.$('.mode-overlay').fadeIn('fast');
+
+      // Reset Group control positions/opacities
+      Ember.$('.group-controller .range-hue').prop('value', 32640);
+  		Ember.$('.group-controller .range-bri').prop('value', 128);
+  		Ember.$('.group-controller .range-sat').prop('value', 128);
+      Ember.$('.group-spectrum-bg, .group-brightness-wrap').css('opacity', 1);
     }
   }
 });
